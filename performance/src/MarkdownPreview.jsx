16bdf40fe78f7d0f5a2817ef49c3cf8a67688dba
Reda <mahmoudreda457@gmail.com>
2026-06-12 07:07:42 +0300

    create a heavy junky app

---
+++ performance/src/MarkdownPreview.jsx
@@ -0,0 +1,20 @@
+const JANK_DELAY = 100;
+
+export default function MarkdownPreview({ render, options }) {
+  const expensiveRender = () => {
+    const start = performance.now();
+    while (performance.now() - start < JANK_DELAY) {}
+    return null;
+  };
+  return (
+    <div>
+      <h1>Last Render: {Date.now()}</h1>
+      <div
+        className="markdown-preview"
+        dangerouslySetInnerHTML={{ __html: render(options.text) }}
+        style={{ color: options.theme }}
+      ></div>
+      {expensiveRender()}
+    </div>
+  );
+}
