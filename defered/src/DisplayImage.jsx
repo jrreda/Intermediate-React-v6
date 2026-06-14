83e5d708c3f8ce30ea71ebdc96fad73f0cd10e84
Reda <mahmoudreda457@gmail.com>
2026-06-14 07:59:25 +0300

    useDeferredValue

--- defered/src/DisplayImage.jsx
+++ defered/src/DisplayImage.jsx
@@ -1,8 +1,9 @@
+import { memo } from "react";
 import img from "../images/luna.jpg";
 
 const JANK_DELAY = 100;
 
-export default function DisplayImage({ filterStyle }) {
+export default memo(function DisplayImage({ filterStyle }) {
   const expensiveRender = () => {
     const start = performance.now();
     while (performance.now() - start < JANK_DELAY) {}
@@ -16,4 +17,4 @@ export default function DisplayImage({ filterStyle }) {
       <p>Last render: {Date.now()}</p>
     </>
   );
-}
+})
