746a3e0fd5020a970911870f2e9360da9925ed40
Reda <mahmoudreda457@gmail.com>
2026-06-14 06:59:01 +0300

    useTransition

--- transitions/src/App.jsx
+++ transitions/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useTransition } from "react";
 import Score from "./Score";
 import getScore from "./getScore";
 
 export default function App() {
-  const [isPending, setIsPending] = useState(true);
+  const [isPending, startTransition] = useTransition(true);
   const [game, setGame] = useState(1);
   const [score, setScore] = useState({ home: "–", away: "–" });
 
   async function getNewScore(game) {
-    setIsPending(true);
     setGame(game);
-    const newScore = await getScore(game);
-    setScore(newScore);
-    setIsPending(false);
+    startTransition(async () => {
+      const newScore = await getScore(game);
+      console.log("game:", game);
+
+      // startTransition(() => {
+        setScore(newScore);
+      // });
+    });
   }
 
   useEffect(() => {
@@ -23,7 +27,6 @@ export default function App() {
     <div className="app">
       <h1>Game {game}</h1>
       <select
-        disabled={isPending}
         onChange={(e) => {
           getNewScore(e.target.value);
         }}
